8ab6ab87a5aca7bb3fb179a4ed7765c0a3a72a50
Anastasia Aravina <aaquariuss13@gmail.com>
2025-05-17 15:46:41 +0300

    Initial commit

---
+++ src/test/java/com/aravina/cakes/CakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aravina.cakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
